36c74dc09c08672392f43bcaeff3cd5a4ef99847
ganjAI <goeckeaaron@gmail.com>
2023-03-07 14:04:46 +0100

    renamed:    count.py -> statistics/count.py

--- test.py
+++ test.py
@@ -3,6 +3,5 @@ from torchvision import datasets, transforms
 from Models import fcbformer
 import torchvision
 from PIL import Image
-model = fcbformer.FCBFormer()
 
 print(torch.cuda.device_count())
